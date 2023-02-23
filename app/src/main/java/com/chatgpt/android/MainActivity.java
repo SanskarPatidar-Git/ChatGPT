1b18c0c95c1304d136d76ec4d1930b07dd8948ad
sanskar patidar <sanskarpatidar14@gmail.com>
2023-02-23 13:57:01 +0530

    Initial commit

---
+++ app/src/main/java/com/chatgpt/android/MainActivity.java
@@ -0,0 +1,16 @@
+package com.chatgpt.android;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        System.out.println("applicationContext "+BaseApplication.baseApplication);
+        System.out.println("applicationContext "+getApplicationContext());
+    }
+}
